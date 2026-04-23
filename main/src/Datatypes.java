1f884ac8257c6da654b1d1cc68173612d53c24e7
JuanCruzSonzini <juancruzsonzini55@gmail.com>
2026-04-22 23:47:29 -0300

    Subo hasta operadores, hora 2:02 curso brais

--- main/src/Datatypes.java
+++ main/src/Datatypes.java
@@ -1,2 +1,14 @@
 public class Datatypes {
+    public static void main(String [] args) {
+        int myInt=37;
+        double myDouble=3.14; //float, long, byte
+        char myChar='A';
+        boolean myBoolean=true;
+
+        //string no es primitivo
+        String myString="Hello World";
+
+        //tipo de dato en tiempo de compilacion
+        System.out.println(myString.getClass().getSimpleName());
+    }
 }
---
+++ main/src/Operadores.java
@@ -0,0 +1,28 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //operadores
+
+        //aritmeticos
+        var a=5;
+        var b=3;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        //asignacion
+        a = b;
+        System.out.println(a);
+
+        a = b*2;
+        System.out.println(a);
+
+        a += 1; // a -=1, a *=2, a /=2, a %=2 son otros casos
+        System.out.println(a);
+
+        //Comparacion
+
+    }
+}
